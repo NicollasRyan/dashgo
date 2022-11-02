5128f9d503a09d4430991b695d3f0fe874044141
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-11-02 10:57:40 -0300

    paging component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -101,7 +101,11 @@ export default function UserList() {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={15}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>
